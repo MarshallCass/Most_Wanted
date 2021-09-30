20dd977896c05303ca73ae34a1dadc97bf097b11
Marshall C <MCass2101@gmail.com>
2021-09-29 20:01:29 -0400

    Added search by name callback after multi name display result

--- app.js
+++ app.js
@@ -183,7 +183,7 @@ function searchByOccupation(people) {
 function displayPeople(people) {
   alert(people.map(function (person) {
     return person.firstName + " " + person.lastName;
-  }).join("\n"));
+  }).join("\n")), searchByName(people);
 }
 
 function displayPerson(person) {
